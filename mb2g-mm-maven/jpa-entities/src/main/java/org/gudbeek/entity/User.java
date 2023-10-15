fa3afca7b0a93ab5a2df54c357043d7767c2862d
nehas <nehasingh.desk@gmail.com>
2023-10-15 22:57:10 +0530

    Multi modules maven

---
+++ mb2g-mm-maven/jpa-entities/src/main/java/org/gudbeek/entity/User.java
@@ -0,0 +1,23 @@
+package org.gudbeek.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+}
